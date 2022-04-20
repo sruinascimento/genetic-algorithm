3162e20fe59e4db52149bef392af9cc457e0a52e
sruiNascimento <sruy19@gmail.com>
2022-04-19 22:13:39 -0300

    Individuo updated

--- 8-Queen/Individuo.py
+++ 8-Queen/Individuo.py
@@ -6,12 +6,11 @@ class Individuo:
         self.chromosome = self.generate_chromosome() if not chromosome else chromosome
 
     def generate_chromosome(self) -> list:
-        ''' Generate matriz 8X8 with 8 Queens'''
+        ''' Generate matriz 8X8 with 8 Queens, [1] - Queen '''
         random_chromosome = np.zeros(shape=(8,8),dtype=np.int64)
         coordinates = []
         while len(coordinates) < 8:
-            x_coordinate = np.random.randint(0, 7)
-            y_coordinate = np.random.randint(0, 7)
+            x_coordinate, y_coordinate = np.random.randint(8, size=(2))
             random_cordinate = (x_coordinate, y_coordinate)
             if random_cordinate not in coordinates:
                 coordinates.append(random_cordinate)
