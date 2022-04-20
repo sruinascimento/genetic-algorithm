ffe60eb9986e4b462602a36a8ad904710414159a
sruiNascimento <sruy19@gmail.com>
2022-04-19 22:05:11 -0300

    class Individuo updated wiht method to generate chromosome

---
+++ 8-Queen/Individuo.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+class Individuo: 
+
+    def __init__(self, chromosome = False ) -> None:
+        self.chromosome = self.generate_chromosome() if not chromosome else chromosome
+
+    def generate_chromosome(self) -> list:
+        ''' Generate matriz 8X8 with 8 Queens'''
+        random_chromosome = np.zeros(shape=(8,8),dtype=np.int64)
+        coordinates = []
+        while len(coordinates) < 8:
+            x_coordinate = np.random.randint(0, 7)
+            y_coordinate = np.random.randint(0, 7)
+            random_cordinate = (x_coordinate, y_coordinate)
+            if random_cordinate not in coordinates:
+                coordinates.append(random_cordinate)
+        
+        for coordinate in coordinates:
+            random_chromosome[coordinate[0]][coordinate[1]] = 1
+
+        return random_chromosome
+
+    def __repr__(self) -> str:
+        return f'chromossome:\n{self.chromosome}'
+
+
+if  __name__ == '__main__':
+    i1 = Individuo()
+    print(i1)
